a7b6458e3adb0d18b43f554519bb85417b38b82a
vale981 <vale981@gmx.de>
2013-11-16 19:07:00 +0100

    Adding some useless stuff to main.cpp...

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
  */
 
 #include <cstdlib>
-
+#include <string>
+#include <iostream>
 using namespace std;
 
 /*
@@ -14,6 +15,7 @@ using namespace std;
  */
 int main(int argc, char** argv) {
 
+    cout << "Hi";
     return 0;
 }
 
