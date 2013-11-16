09527d36c41a64f9f4454721ae7689804228b29f
vale981 <vale981@gmx.de>
2013-11-16 19:03:24 +0100

    dlldld

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: vale981
+ *
+ * Created on 16. November 2013, 19:02
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
